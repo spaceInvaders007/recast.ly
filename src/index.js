84b3c59ce3926d51dbeee48d01744f3e00cc5b6f
NapoleonDLP <napoleon.dlp@gmail.com>
2019-04-23 11:18:54 -0700

    vieos rendering to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-// TODO: Render the `App` component to the DOM
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+
+ReactDOM.render(<App />, document.getElementById('app'));
